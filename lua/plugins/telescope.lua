e6f61f031137782e5c01d2e0e225d5c0b95f401a
balou <balou@192.168.1.24>
2025-03-03 01:05:13 +0000

    fix(performance): optimization for two heavy plugins

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,9 +1,38 @@
 return { 
-	"nvim-telescope/telescope.nvim", tag = '0.1.8',
-	dependencies = { 'nvim-lua/plenary.nvim' },
+	"nvim-telescope/telescope.nvim",
+	tag = '0.1.8',
+	dependencies = { 
+		'nvim-lua/plenary.nvim',
+		{ 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' }
+	},
+	cmd = "Telescope",
+	keys = {
+		{ '<C-f>', function() require('telescope.builtin').find_files() end },
+		{ '<leader>f', function() require('telescope.builtin').live_grep() end },
+	},
 	config = function()
-		local builtin = require("telescope.builtin")
-		vim.keymap.set('n', '<C-f>', builtin.find_files, {})
-		vim.keymap.set('n', '<leader>f', builtin.live_grep, {})
+		local telescope = require("telescope")
+		telescope.setup({
+			defaults = {
+				file_ignore_patterns = { ".git/", "node_modules/" },
+				path_display = { "truncate" },
+				set_env = { COLORTERM = "truecolor" },
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--hidden",
+				},
+			},
+			pickers = {
+				find_files = { theme = "dropdown" },
+				live_grep = { theme = "dropdown" },
+			},
+		})
+		telescope.load_extension('fzf')
 	end
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -2,12 +2,30 @@
 return { 
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	event = { "BufReadPost", "BufNewFile" },
 	config = function()
-		local config =  require("nvim-treesitter.configs")
+		vim.opt.foldmethod = "expr"
+		vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
+		vim.opt.foldenable = false
+		local config = require("nvim-treesitter.configs")
 		config.setup({
 			ensure_installed = {"lua", "python"},
-			highlight = {enable = true},
-			indent = {enable = true},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = {
+				enable = true
+			},
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "gnn",
+					node_incremental = "grn",
+					scope_incremental = "grc",
+					node_decremental = "grm",
+				},
+			},
 		})
 	end
 }
