d0f897b563d145c7f781054b08585d06d2653ca1
balou <balou@192.168.1.24>
2025-03-03 01:09:55 +0000

    feat(vim-options): add memory for undo and redo

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -1,1 +1,15 @@
 vim.g.mapleader = " "
+
+-- Configuration de la persistance de l'historique
+local undodir = vim.fn.expand('~/.local/share/nvim/undodir')
+
+-- Créer le répertoire s'il n'existe pas
+if vim.fn.isdirectory(undodir) == 0 then
+    vim.fn.mkdir(undodir, 'p')
+end
+
+-- Activer la persistance de l'historique
+vim.opt.undofile = true
+vim.opt.undodir = undodir
+vim.opt.undolevels = 1000
+vim.opt.undoreload = 10000
